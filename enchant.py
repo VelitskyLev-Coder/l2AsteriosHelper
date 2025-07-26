8118107200fa863aacbd0a07a94eb68dc779d95f
velitskylev <velitskylev@gmail.com>
2025-07-26 23:41:23 +0300

    enc sim

--- enchant.py
+++ enchant.py
@@ -22,15 +22,15 @@ def trial_batch(args) -> list[int]:
 
 def simulate_enchant(trials: int, target: int, success_prob: float, processes: int = 25, batch_size: int = 1000) -> list[int]:
     if trials % 1000 != 0:
-        raise ValueError("trials size must be devitalise by 1000")
+        raise ValueError("trials size must be multiple off 1000")
     batches = [(target, success_prob, batch_size)] * (trials // batch_size)
     with Pool(processes=processes) as pool:
         chunks = list(tqdm(pool.imap_unordered(trial_batch, batches), total=len(batches)))
     return [x for chunk in chunks for x in chunk]  # flatten
 
 def main() -> None:
-    target = 16
-    success_prob = 0.50
+    target = 10
+    success_prob = 0.66
     trials = 1_000_000
     results = simulate_enchant(trials, target, success_prob)
 
@@ -47,11 +47,11 @@ def main() -> None:
 
     plt.figure(figsize=(10, 6))
     plt.plot(sorted_results, cumulative_prob, label="Cumulative Probability")
-    plt.axhline(0.5, color='red', linestyle='dashed', label=f'Median: {median_val:.0f}')
-    plt.axhline(0.9, color='orange', linestyle='dashed', label=f'90% ≤: {p90_val:.0f}')
-    plt.axhline(0.99, color='green', linestyle='dashed', label=f'99% ≤: {p99_val:.0f}')
-    plt.axhline(0.999, color='purple', linestyle='dashed', label=f'99.9% ≤: {p99_9_val:.0f}')
-    plt.axvline(mean_val, color='blue', linestyle='dotted', label=f'Average: {mean_val:.0f}')
+    plt.axhline(0.5, color='red', linestyle='dashed', label=f'Median: {median_val:.2f}')
+    plt.axhline(0.9, color='orange', linestyle='dashed', label=f'90% ≤: {p90_val:.2f}')
+    plt.axhline(0.99, color='green', linestyle='dashed', label=f'99% ≤: {p99_val:.2f}')
+    plt.axhline(0.999, color='purple', linestyle='dashed', label=f'99.9% ≤: {p99_9_val:.2f}')
+    plt.axvline(mean_val, color='blue', linestyle='dotted', label=f'Average: {mean_val:.2f}')
     plt.xlabel("Number of Blessed Scrolls")
     plt.xticks(np.linspace(0, max(sorted_results), 20, dtype=int))
     plt.ylabel("Cumulative Probability")
